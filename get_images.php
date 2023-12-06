d39b17efc9fe80802f372aa5fb62e622bedc852f
HarpreetSingh <84021730+HarpreetS1@users.noreply.github.com>
2023-12-06 21:17:39 +0530

    Update get_images.php

--- get_images.php
+++ get_images.php
@@ -1,17 +1,11 @@
 <?php
 
 $uploadsDir = 'uploads/';
-
-// Get all files in the uploads directory
 $files = scandir($uploadsDir);
-
-// Filter out "." and ".." directories
 $files = array_diff($files, array('.', '..'));
 
-// Create an array to store image information
 $images = [];
 
-// Iterate through the files
 foreach ($files as $file) {
     $images[] = [
         'filename' => $file,
@@ -19,7 +13,6 @@ foreach ($files as $file) {
     ];
 }
 
-// Output the array as JSON
 header('Content-Type: application/json');
 echo json_encode($images);
 ?>
