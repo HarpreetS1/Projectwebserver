c6d2433a0c0700eb1363781e4a33e8c9b22f1886
HarpreetSingh <84021730+HarpreetS1@users.noreply.github.com>
2023-12-06 22:03:59 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,18 +1,29 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Handle photo upload
+    $fileFieldName = "file";
+    $targetDir = "uploads/";
+    $targetFile = $targetDir . basename($_FILES[$fileFieldName]["name"]);
 
-$uploadsDir = 'uploads/';
-
-// Check if the 'file' parameter exists in the POST request
-if (isset($_FILES["file"])) {
-    $targetFile = $uploadsDir . basename($_FILES["file"]["name"]);
-
-    // Move the uploaded file to the target directory
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
-        echo "File uploaded successfully!";
+    if (move_uploaded_file($_FILES[$fileFieldName]["tmp_name"], $targetFile)) {
+        echo "Photo uploaded successfully.";
     } else {
-        echo "Error uploading file.";
+        echo "Error uploading photo.";
     }
+
+    // Get GPS data
+    $latitude = isset($_SERVER["HTTP_LATITUDE"]) ? $_SERVER["HTTP_LATITUDE"] : "N/A";
+    $longitude = isset($_SERVER["HTTP_LONGITUDE"]) ? $_SERVER["HTTP_LONGITUDE"] : "N/A";
+
+    echo "Latitude: " . $latitude . "<br>";
+    echo "Longitude: " . $longitude . "<br>";
+
+    // Save GPS data to a database or perform other actions as needed
+    // ...
+
+    // Display map with location
+    echo '<iframe width="600" height="450" frameborder="0" style="border:0" src="https://maps.google.com?q=' . $latitude . ',' . $longitude . '&output=embed"></iframe>';
 } else {
-    echo "No file attached to the request.";
+    echo "Invalid request.";
 }
 ?>
