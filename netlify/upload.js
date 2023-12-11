f017215166cefa8623af3682067050bf30abf5df
TheCaptain333 <106683739+TheCaptain333@users.noreply.github.com>
2023-12-11 23:38:40 +0530

    Update upload.js

--- netlify/upload.js
+++ netlify/upload.js
@@ -6,6 +6,47 @@ const fileUpload = require('express-fileupload');
 const app = express();
 app.use(fileUpload());
 
+// exports.handler = async (event, context) => {
+//   if (event.httpMethod !== 'POST') {
+//     return {
+//       statusCode: 400,
+//       body: 'Invalid request method.',
+//     };
+//   }
+
+//   const fileFieldName = 'file';
+//   const targetDir = 'uploads/';
+//   const targetFile = targetDir + JSON.parse(event.body)[fileFieldName].name;
+
+//   const fileBuffer = Buffer.from(JSON.parse(event.body)[fileFieldName].data, 'base64');
+
+//   // Handle photo upload
+//   try {
+//     require('fs').writeFileSync(targetFile, fileBuffer);
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: 'Error uploading photo.',
+//     };
+//   }
+
+//   // Get GPS data
+//   const latitude = event.headers.latitude || 'N/A';
+//   const longitude = event.headers.longitude || 'N/A';
+
+//   // Save GPS data to a database or perform other actions as needed
+//   // ...
+
+//   // Display map with location
+//   const mapIframe = `<iframe width="600" height="450" frameborder="0" style="border:0" src="https://maps.google.com?q=${latitude},${longitude}&output=embed"></iframe>`;
+//   console.log(mapIframe);
+
+//   return {
+//     statusCode: 200,
+//     body: `Photo uploaded successfully.\nLatitude: ${latitude}\nLongitude: ${longitude}`,
+//   };
+// };
+
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return {
@@ -14,15 +55,19 @@ exports.handler = async (event, context) => {
     };
   }
 
-  const fileFieldName = 'file';
+  if (!event.files) {
+    return {
+      statusCode: 400,
+      body: 'No file uploaded.',
+    };
+  }
+
+  const file = event.files[fileFieldName];
   const targetDir = 'uploads/';
-  const targetFile = targetDir + JSON.parse(event.body)[fileFieldName].name;
+  const targetFile = targetDir + file.name;
 
-  const fileBuffer = Buffer.from(JSON.parse(event.body)[fileFieldName].data, 'base64');
-
-  // Handle photo upload
   try {
-    require('fs').writeFileSync(targetFile, fileBuffer);
+    await file.mv(targetFile);
   } catch (error) {
     return {
       statusCode: 500,
@@ -30,19 +75,14 @@ exports.handler = async (event, context) => {
     };
   }
 
-  // Get GPS data
   const latitude = event.headers.latitude || 'N/A';
   const longitude = event.headers.longitude || 'N/A';
 
-  // Save GPS data to a database or perform other actions as needed
   // ...
-
-  // Display map with location
-  const mapIframe = `<iframe width="600" height="450" frameborder="0" style="border:0" src="https://maps.google.com?q=${latitude},${longitude}&output=embed"></iframe>`;
-  console.log(mapIframe);
 
   return {
     statusCode: 200,
     body: `Photo uploaded successfully.\nLatitude: ${latitude}\nLongitude: ${longitude}`,
   };
 };
+
