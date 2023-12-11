781a932f7b4b7d2d8b6772c84146ed2917f97c54
TheCaptain333 <106683739+TheCaptain333@users.noreply.github.com>
2023-12-12 00:29:14 +0530

    Update get_images.js

--- netlify/get_images.js
+++ netlify/get_images.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const uploadsDir = 'Projectwebserver/uploads/';
+const uploadsDir = 'projectwebserver/uploads/';
 
 exports.handler = async (event, context) => {
   return new Promise((resolve, reject) => {
