c0744b950e2cb6978b809d6aacfece019cd2d9cd
HarpreetSingh <84021730+HarpreetS1@users.noreply.github.com>
2023-12-13 12:33:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,32 +14,23 @@ app.post('/upload', upload.single('photo'), (req, res) => {
   try {
     // Get the uploaded image data
     const imageBuffer = req.file.buffer;
-    
-    // Generate a unique filename
-    const filename = `photo_${Date.now()}.jpg`;
-//rename old file
-    fs.rename( './uploads/latest-image.png}', `./uploads/${filename}`, callback )
-    // Save the image to the server
-    fs.writeFileSync(`./uploads/latest-image.png}`, imageBuffer);
 
-    console.log(`Image saved: latest-image.png `);
+    // Save the image with the name "latest-image.png"
+    fs.writeFileSync('./uploads/latest-image.png', imageBuffer);
+
+    console.log('Image saved: latest-image.png');
     res.status(200).send('Image received and saved successfully.');
   } catch (error) {
     console.error('Error handling image upload:', error);
     res.status(500).send('Internal Server Error');
   }
 });
+
 // Endpoint to fetch the latest uploaded image
 app.get('/latest-image', (req, res) => {
   try {
-    // Read the list of files in the 'uploads' folder
-    const files = fs.readdirSync('./uploads');
-
-    // Get the latest image (if available)
-    const latestImage = files.length > 0 ? files[files.length - 1] : null;
-
     // Provide the URL of the latest image
-    const imageUrl = latestImage ? `/uploads/${latestImage}` : null;
+    const imageUrl = '/uploads/latest-image.png';
 
     res.json(imageUrl);
   } catch (error) {
