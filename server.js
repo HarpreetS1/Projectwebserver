7bee76696faba61e7d130d7c8a49a65bc2603a05
HarpreetSingh <84021730+HarpreetS1@users.noreply.github.com>
2023-12-13 06:44:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,24 @@ app.post('/upload', upload.single('photo'), (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+// Endpoint to fetch the latest uploaded image
+app.get('/latest-image', (req, res) => {
+  try {
+    // Read the list of files in the 'uploads' folder
+    const files = fs.readdirSync('./uploads');
+
+    // Get the latest image (if available)
+    const latestImage = files.length > 0 ? files[files.length - 1] : null;
+
+    // Provide the URL of the latest image
+    const imageUrl = latestImage ? `/uploads/${latestImage}` : null;
+
+    res.json(imageUrl);
+  } catch (error) {
+    console.error('Error fetching latest image:', error);
+    res.status(500).json(null);
+  }
+});
 
 // Start the server
 app.listen(port, () => {
